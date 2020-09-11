08986096342fa98e3b522c8cadaced3e7b40b3d3
Dima-Kasheev <52133651+Dima-Kasheev@users.noreply.github.com>
2020-09-11 15:12:48 +0300

    Create 2.py

---
+++ DZ-1/2.py
@@ -0,0 +1,11 @@
+a = float(input('a: '))
+b = float(input('b: '))
+c = float(input('c: '))
+f = float(input('f: '))
+d = float(input('d: '))
+g = f - d
+
+if g:
+    print ((a*b - c) / g)
+else:
+    print ('ne delit')
