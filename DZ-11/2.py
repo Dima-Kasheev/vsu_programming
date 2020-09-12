c3cedbc6d3d7f7cbabf2c42c8fdb0e1951bc1b4f
Dima-Kasheev <52133651+Dima-Kasheev@users.noreply.github.com>
2020-09-12 17:30:45 +0300

    Delete 2.py

--- DZ-11/2.py
+++
@@ -1,7 +0,0 @@
-from Fraction import Fraction
-
-
-class IrreducibleFraction(Fraction):
-    def __init__(self, numerator=0, denominator=1):
-        super().__init__(numerator, denominator)
-        self.reduce()
